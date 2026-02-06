323f23bde6557f5bb7791cef4e5636608a12203b
yyk <yunyoung0914@gmail.com>
2026-02-06 16:30:14 +0900

    day3

--- 3일차/색종이의총넓이.java
+++ 3일차/색종이의총넓이.java
@@ -13,10 +13,22 @@ public class 색종이의총넓이 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		int[] a = new int[N];
+		int[] b = new int[N];
+
 		for (int i=0; i<N; i++) {
-				
+			int a[i] = sc.nextInt();
+			int b[i] = sc.nextInt();
+		}
+
+		for (int x=-100; x<=100; x++) {
+			for (int y=-100; y<=100; y++) {
+				for (int i=0; i<N; i++) {
+					if (
+						isIn(x, y, a[i], b[i]) || isIn()
+
+				}
+			}
 		}
 	}
-
-
 }
