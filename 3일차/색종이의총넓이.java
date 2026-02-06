5b3aa6d1b599c938c1a19dbafe300ad7797fb377
yyk <yunyoung0914@gmail.com>
2026-02-06 16:23:18 +0900

    day3

---
+++ 3일차/색종이의총넓이.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class 색종이의총넓이 {
+	public static boolean isIn(int x, int y, int A, int B) {
+		if ((x>=A && x<A+8) && (y>=B y<B+8)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i=0; i<N; i++) {
+				
+		}
+	}
+
+
+}
